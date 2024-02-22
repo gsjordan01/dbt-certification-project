5104b4a1093ee84e969c01296bd8e9ec0783d4cd
Jordan GS <jordan_guzman@hakkoda.io>
2024-02-22 15:25:18 +0000

    finished intermediate models

--- dbt/models/intermediate/int_users_countries_joined.sql
+++ dbt/models/intermediate/int_users_countries_joined.sql
@@ -1,28 +1,29 @@
 with
 
 users as (
-
   select * from {{ ref('stg_boardgames__users') }}
+),
 
+countries_user_ref as (
+  select * from {{ ref('stg_country_codes_user_ref') }}
 ),
 
 countries as (
+  select * from {{ ref('stg_country_codes') }}
+)
 
-  select * from {{ ref('stg_country_codes_user_ref') }}
 
-),
-
-final as (
+user_countries_joined as (
 
   select
-    u.user_name,
-    u.user_url,
-    c.country_code
-
-  from users u
-  join
-  countries c on u.country_name = c.country_name
+    users.user_name,
+    users.user_url,
+    coalesce(countries.country_name, countries_user_ref.country_name) as country_name,
+    coalesce(countries.country_code, countries_user_ref.country_code) as country_code
+  from users
+  left join countries_user_ref on users.country_name - countries_user_ref.country_name
+  left join countries on users.country_name = countries.country_name
 
 )
 
-select * from final
+select * from user_countries_joined
