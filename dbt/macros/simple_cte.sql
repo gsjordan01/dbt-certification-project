a82c2d8995270ee22d191d9ac0aa6d14702b68e2
Jordan GS <jordan_guzman@hakkoda.io>
2024-02-23 19:20:33 +0000

    example of a simple macro created

---
+++ dbt/macros/simple_cte.sql
@@ -0,0 +1,11 @@
+{% macro simple_cte(tuple_list) %}
+
+with {% for cte_ref in tuple_list %} {{cte_ref[0]}} as (
+  select * from {{ ref(cte_ref[1]) }}
+)
+{%- if not loop.last -%}
+,
+{%- endif -%}
+{%- endfor -%}
+
+{%- endmacro %}
---
+++ dbt/models/intermediate/example.sql
@@ -0,0 +1,13 @@
+{{
+  simple_cte(
+    [
+      ('reviews', 'stg_boardgames__reviews'),
+      ('users', 'stg_boardgames__users')
+    ]
+  )
+}}
+
+select * 
+from reviews
+join users 
+  on reviews.review_username = users.user_username
